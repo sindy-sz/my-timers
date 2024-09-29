25aa40742776e40d64994a0c036fad8a06bc5c26
Sindy Zaki <sindy.zaki@gmail.com>
2024-09-29 15:46:09 -0400

    create a countdown timer - wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
+import AvTimerIcon from '@mui/icons-material/AvTimer';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField'; 
+import Button from '@mui/material/Button'
 import { useState } from 'react'
+import Typography from '@mui/material/Typography';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [duration, setDuration] = useState<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState<number | null>(duration);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Stack spacing={2}>
+
+      <Typography variant="h3">My Timers</Typography>
+
+      <TextField label="Timer Duration (seconds)" 
+                type="number" 
+                value={duration??""}
+                onChange={evt => setDuration(+evt.target.value)}
+      />
+      
+      <Button variant="contained" 
+              endIcon={<AvTimerIcon />}
+              onClick={() => {
+                setRemainingTime(duration);
+                setInterval(() => {
+                  setRemainingTime((previous) => {
+                    let next = previous! - .5;
+                    return next;
+                  });
+                }, 500)}
+              }
+              >
+        Start Timer
+      </Button>
+
+      <Typography variant="h3">
+        { remainingTime }
+      </Typography>
+
+    </Stack>
   )
 }
 
